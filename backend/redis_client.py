27fc1ef26bb900ab556531004e126088b10fd975
Bhavya-2005 <bhavya21298@gmail.com>
2026-05-14 13:16:16 +0530

    Disabled Redis for deployment

--- backend/redis_client.py
+++ backend/redis_client.py
@@ -1,28 +1,2 @@
-import redis
-import json
-
-redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
-    decode_responses=True
-)
-
-
 def store_orderbook(data):
-
-    redis_client.set(
-        "orderbook",
-        json.dumps(data)
-    )
-
-
-def get_orderbook():
-
-    data = redis_client.get(
-        "orderbook"
-    )
-
-    if data:
-        return json.loads(data)
-
-    return {}
+    pass
